4b8b0da1f02f2267e5bb6498fb6d1495fad37157
pemweb1 <aniendyao@gmail.com>
2025-04-26 12:33:40 +0700

    update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Profil::home');
+$routes->get('/matkul/(:num)', 'Profil::matkul/$1');
+
 $routes->get('pemweb', 'Profil::pemweb');
 $routes->get('mbd', 'Profil::mbd');
 $routes->get('mjk', 'Profil::mjk');
