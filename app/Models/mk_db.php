39bdfdf6914792fc5ac7a21379deaadf18cd928a
pemweb1 <aniendyao@gmail.com>
2025-04-26 12:34:25 +0700

    update models

--- app/Models/mk_db.php
+++ app/Models/mk_db.php
@@ -6,18 +6,16 @@ use CodeIgniter\Model;
 
 class mk_db extends Model
 {
-    protected $table = 'courses';
+    protected $table = 'matkul';
     protected $primaryKey = 'id';
 
     protected $allowedFields = [
-        'name',
-        'description',
-        'goals',
-        'sks',
-        'lecturer',
-        'schedule',
-        'classroom_link',
-        'wa_link',
-        'image'
+        'deskripsi',
+        'gambar',
+        'nama_mk',
+        'jam',
+        'dosen',
+        'link_gc',
+        'link_wa'
     ];
 }
